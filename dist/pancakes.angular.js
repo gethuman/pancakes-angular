9574c1882e0bd1491abdf33d2a9f391c4889fd49
Jeff Whelpley <jeff@gethuman.com>
2014-11-12 16:18:51 -0500

    Added facebook hash fix

--- dist/pancakes.angular.js
+++ dist/pancakes.angular.js
@@ -5,6 +5,15 @@
  * The app definition for pancakes.angular
  */
 angular.module('pancakesAngular', []);
+
+// this little hack is used to fix issue with facebook social auth
+// TODO: put this someplace better, but won't hurt to have it here for now
+// really, this belongs in security plugin once I get to those
+if (window.location.hash === '#_=_') {
+    history.replaceState ?
+        history.replaceState(null, null, window.location.href.split('#')[0]) :
+        window.location.hash = '';
+}
 /**
  * Author: Jeff Whelpley
  * Date: 10/16/14
