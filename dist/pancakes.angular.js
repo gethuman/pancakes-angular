7fb9bcf397bf3efa1301b9b508bde5455c4c3701
Jeff Whelpley <jeff@gethuman.com>
2015-03-02 13:34:51 -0500

    Fixing issue with cookies and storage

--- dist/pancakes.angular.js
+++ dist/pancakes.angular.js
@@ -4,7 +4,7 @@
  *
  * The app definition for pancakes.angular
  */
-angular.module('pancakesAngular', ['ngCookies']);
+angular.module('pancakesAngular', []);
 
 /* jshint undef: false */
 
@@ -1304,8 +1304,32 @@ angular.module('pancakesAngular').provider('stateLoader', function () {
  *
  * This is used to store stuff in localStorage and cookies at same time
  */
-angular.module('pancakesAngular').factory('storage', function (extlibs, $cookies, $document) {
+angular.module('pancakesAngular').factory('storage', function (extlibs, config) {
     var localStorage = extlibs.get('localStorage');
+    var cookieDomain = config.cookieDomain;
+    var document = window.document;
+
+    /**
+     * Set a cookie value
+     * @param name
+     * @param value
+     * @param domain
+     * @returns {boolean}
+     */
+    function setCookie(name, value, domain) {
+        if (!name || /^(?:expires|max\-age|path|domain|secure)$/i.test(name)) { return false; }
+
+        // infinity
+        var expires = "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+        var path = '/';
+        var isSecure = false;
+
+        document.cookie = encodeURIComponent(name) +
+            "=" + encodeURIComponent(value) + expires +
+            (domain ? "; domain=" + domain : "") +
+            (path ? "; path=" + path : "") +
+            (isSecure ? "; secure" : "");
+    }
 
     /**
      * Set a value in both localStorage and cookies
@@ -1314,7 +1338,23 @@ angular.module('pancakesAngular').factory('storage', function (extlibs, $cookies
      */
     function set(name, value) {
         localStorage.setItem(name, value);
-        $cookies[name] = value;
+        setCookie(name, value, cookieDomain);
+    }
+
+    /**
+     * Helper function to get a cookie
+     * @param name
+     * @returns {*}
+     */
+    function getCookie(name) {
+        if (!name) { return null; }
+        return decodeURIComponent(
+                document.cookie.replace(
+                    new RegExp("(?:(?:^|.*;)\\s*" +
+                    encodeURIComponent(name).replace(/[\-\.\+\*]/g, "\\$&") +
+                    "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1"
+                )
+            ) || null;
     }
 
     /**
@@ -1323,7 +1363,33 @@ angular.module('pancakesAngular').factory('storage', function (extlibs, $cookies
      * @param name
      */
     function get(name) {
-        return localStorage.getItem(name) || $cookies[name];
+        return localStorage.getItem(name) || getCookie(name);
+    }
+
+    /**
+     * Check to see if a cookie exists
+     * @param name
+     * @returns {boolean}
+     */
+    //function cookieExists(name) {
+    //    if (!name) { return false; }
+    //    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(name).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    //}
+
+    /**
+     * Remove a cookie
+     * @param name
+     * @param domain
+     * @returns {boolean}
+     */
+    function removeCookie(name, domain) {
+        //if (!cookieExists(name)) { return false; }
+
+        var path = '/';
+        document.cookie = encodeURIComponent(name) +
+            "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
+            (domain ? "; domain=" + domain : "") +
+            (path ? "; path=" + path : "");
     }
 
     /**
@@ -1332,12 +1398,15 @@ angular.module('pancakesAngular').factory('storage', function (extlibs, $cookies
      */
     function remove(name) {
         localStorage.removeItem(name);
-        window.document.cookie = name + '=; path=/; domain=.gethuman.com; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+        removeCookie(name, cookieDomain);
     }
 
     return {
+        setCookie: setCookie,
         set: set,
+        getCookie: getCookie,
         get: get,
+        removeCookie: removeCookie,
         remove: remove
     };
 });
