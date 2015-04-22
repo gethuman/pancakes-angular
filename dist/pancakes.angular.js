b55872b758685f60286d38648f524bb64d8b897e
Jeff Whelpley <jeff@gethuman.com>
2015-04-22 16:41:39 -0400

    Updating client logging to use sentry

--- dist/pancakes.angular.js
+++ dist/pancakes.angular.js
@@ -643,33 +643,60 @@ angular.module('pancakesAngular').factory('eventBus', function ($document, $root
  *
  * Listens for log events and sends them to the console
  */
-angular.module('pancakesAngular').factory('clientLogReactor', function (eventBus) {
+angular.module('pancakesAngular').factory('clientLogReactor', function (extlibs, eventBus, config) {
+    config = config || {};
+
+    var raven = extlibs.get('Raven');
+    var useConsole = config.logTransport && config.logTransport.indexOf('console') >= 0;
+    var useRemote = raven && config.logTransport && config.logTransport.indexOf('remote') >= 0;
+
+    if (raven) {
+        raven.config(config.errorUrl, {}).install();
+    }
 
     /**
      * Send log to the console
      * @param event
      * @param logData
      */
-    function logToConsole(event, logData) {
-        console.log(logData + ' || ' + JSON.stringify(logData));
+    function log(event, logData) {
+        if (useConsole) {
+            console.log(logData + ' || ' + JSON.stringify(logData));
+        }
+
+        if (useRemote) {
+            var err = logData.err;
+            delete logData.err;
+
+            err ?
+                raven.captureError(err) :
+                angular.isString(logData) ?
+                    raven.captureMessage(logData) :
+                    raven.captureMessage(logData.msg, { extra: logData });
+        }
     }
 
     /******* INIT API & EVENT HANDLERS ********/
 
-    eventBus.on('log.debug', logToConsole);              // listen for the log events
-    eventBus.on('log.info', logToConsole);
-    eventBus.on('log.error', logToConsole);
-    eventBus.on('log.critical', logToConsole);
-    eventBus.on('error.api', logToConsole);
+    eventBus.on('log.debug',    log);
+    eventBus.on('log.info',     log);
+    eventBus.on('log.error',    log);
+    eventBus.on('log.critical', log);
+    eventBus.on('error.api',    log);
 
     // make sure we log any state change errors (only applies to client side)
     eventBus.on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, err) {
-        logToConsole({}, 'State change error: ' + err.stack);
+        eventBus.emit('log.error', {
+            err: err,
+            msg: err + '',
+            stack: err && err.stack,
+            inner: err && err.inner
+        });
     });
 
-    // functions to expose
+    // functions to expose (only for testing purposes)
     return {
-        logToConsole: logToConsole
+        log: log
     };
 });
 /**
