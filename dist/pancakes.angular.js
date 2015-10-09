d679883a58ce65bdb1ffc12ad3f8d5141482e723
Jeff Whelpley <jeff@gethuman.com>
2015-10-09 09:05:19 -0400

    Changing back to recording absolute URL for google analytics

--- dist/pancakes.angular.js
+++ dist/pancakes.angular.js
@@ -666,7 +666,7 @@ angular.module('pancakesAngular').factory('clientAnalytics', ["$window", "$locat
      */
     function captureCurrentPath() {
         var gaq = $window._gaq || [];
-        gaq.push(['_trackPageview', $location.absUrl()]);
+        gaq.push(['_trackPageview', $location.path()]);
     }
 
     // add event handler if the gaq object exists on the window
