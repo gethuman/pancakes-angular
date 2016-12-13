775436c558eec375b0f904e496968409391b9dbf
Jeff Whelpley <jeff@gethuman.com>
2016-12-13 13:37:10 -0500

    updates to ajax

--- dist/pancakes.angular.js
+++ dist/pancakes.angular.js
@@ -179,25 +179,24 @@ angular.module('pancakesAngular')
 
             // finally make the http call
             $http(apiOpts)
-                .success(function (respData) {
+                .then(function (respData) {
                     saveOpts(apiOpts);
                     deferred.resolve(respData);
                 })
-                .error(function (err, status, headers, conf) {
+                .catch(function (err) {
 
-                    // if status between 450 and 500, then assume it can be handled
-                    if (status > 450 && status < 499) {
-                        eventBus.emit('error.' + status, err);
-                        return;
-                    }
+                    // if (status > 450 && status < 499) {
+                    //     eventBus.emit('error.' + status, err);
+                    //     return;
+                    // }
 
                     saveOpts(apiOpts);
-                    var isTimeout = !status || status === -1;
+                    // var isTimeout = !status || status === -1;
 
-                    if (!err && isTimeout) {
-                        err = new Error('Cannot access back end');
-                    }
-                    else if (!err) {
+                    // if (!err && isTimeout) {
+                    //     err = new Error('Cannot access back end');
+                    // }
+                    if (!err) {
                         err = new Error('error httpCode ' + status + ' headers ' +
                             JSON.stringify(headers) + ' conf ' +
                             JSON.stringify(conf));
@@ -207,12 +206,10 @@ angular.module('pancakesAngular')
                         eventBus.emit('error.api', err);
                     }
 
-                    // todo: remove this once have debugged issues
                     log.error(err, {
                         apiOpts: apiOpts,
                         status: status,
-                        headers: headers,
-                        config: conf
+                        headers: headers
                     });
 
                     deferred.reject(err);
@@ -745,78 +742,6 @@ angular.module('pancakesAngular').factory('eventBus', ["$document", "$rootScope"
     return eventBus;
 }]);
 
-/**
- * Author: Jeff Whelpley
- * Date: 2/16/15
- *
- * Listens for log events and sends them to the console
- */
-angular.module('pancakesAngular').factory('clientLogReactor',
-    ["_", "extlibs", "eventBus", "config", function (_, extlibs, eventBus, config) {
-
-        config = config || {};
-
-        // var errorClient = extlibs.get('NREUM');
-        var useConsole = config.logTransport && config.logTransport.indexOf('console') >= 0;
-        // var useRemote = errorClient && config.logTransport && config.logTransport.indexOf('remote') >= 0;
-        var logLevel = config.logLevel || 'error';
-
-        // log to console and to new relic
-        function log(event, logData) {
-            logData = logData || {};
-            // var status = logData.status || 0;
-            // var statusOkToLogRemote = status < 400 || status > 498;
-
-            if (useConsole) {
-                /* eslint no-console:0 */
-                console.log(logData);
-            }
-
-            // log remotely if non-AppError error and remote is active
-            // if (useRemote && logData.msg && statusOkToLogRemote) {
-            //     errorClient.noticeError(new Error(logData.msg));
-            // }
-        }
-
-        /******* INIT API & EVENT HANDLERS ********/
-
-        if (logLevel === 'error') {
-            eventBus.on('log.error',    log);
-            eventBus.on('log.critical', log);
-            eventBus.on('error.api',    log);
-        }
-        else if (logLevel === 'info') {
-            eventBus.on('log.info',     log);
-            eventBus.on('log.error',    log);
-            eventBus.on('log.critical', log);
-            eventBus.on('error.api',    log);
-        }
-        else if (logLevel === 'debug') {
-            eventBus.on('log.debug',    log);
-            eventBus.on('log.info',     log);
-            eventBus.on('log.error',    log);
-            eventBus.on('log.critical', log);
-            eventBus.on('error.api',    log);
-        }
-
-
-        // make sure we log any state change errors (only applies to client side)
-        eventBus.on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, err) {
-            log(event, {
-                err: err,
-                msg: 'state change error from ' + JSON.stringify(fromState) + ' to ' +
-                        JSON.stringify(toState) + ' with error: ' + err + ' ' + JSON.stringify(err),
-                stack: err && err.stack,
-                inner: err && err.inner
-            });
-        });
-
-        // functions to expose (only for testing purposes)
-        return {
-            log: log
-        };
-    }]
-);
 /**
  * Author: Jeff Whelpley
  * Date: 10/15/14
@@ -1353,7 +1278,7 @@ angular.module('pancakesAngular').factory('stateHelper', ["$window", "$timeout",
         return $window.navigator.userAgent;
     }
 
-    // so, this is a total hack, but basically this combination of variables and
+    // so, this is a total hack, but basically this com`bination of variables and
     // event handlers allows us to change the URL without changing the UI router state
     eventBus.on('$stateChangeStart', function (event) {
         if (preventStateChange) {                               // while we are switching URL, don't allow state change
